37fd838c1dace388edb922736579e4eddaacc0d8
Hector-bit <hectormartinez.2978@gmail.com>
2021-12-17 04:23:22 -0800

    first commit, so far yarn android works on my s7

---
+++ components/listingcard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//how do you get images without having to have them all in the app???
+
+function ListingCard(props){
+    console.log('listing card', props);
+    return(
+        <div className='listingcard'>
+            <h3>{props.title}</h3>
+            <img src={props.image}/>
+            <p>{props.description}</p>
+        </div>
+    )
+}
