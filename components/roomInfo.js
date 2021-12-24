f9b8a0b56e2fc576711d1c52dc9f90bc68453965
Hector-bit <hectormartinez.2978@gmail.com>
2021-12-24 09:21:58 -0800

    route id works

--- components/roomInfo.js
+++ components/roomInfo.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { NativeRouter, Routes, Route, Link, useParams } from 'react-router-native';
+import { NativeRouter, Routes, Route, Link, useParams, Outlet} from 'react-router-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { getRoomId } from './databaseFunctions';
 
 function RoomInfo(){
-    let { params } = useParams();
+    let { roomid } = useParams();
     let roominfo = 'nothing'
-    console.log('look at this new room', roominfo, params, params);
+    console.log('look at this new room', roominfo, roomid);
     return (
         <View>
             <Text>
-                nothing to see here, this is a work in progress, room id: { params }
+                nothing to see here, this is a work in progress, room id: { roomid }
             </Text>
+            <Outlet/>
         </View>        
     )
 
